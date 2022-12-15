b65177db35dc6cfb36038936bfc4497246c30a95
Hakkinen Wylgner <Hakkinenwylgner@gmail.com>
2022-12-14 23:14:12 -0300

    Nova alteração no main

--- Main.java
+++ Main.java
@@ -3,16 +3,8 @@ public class Main {
 	public static void main(String[] args) {
 System.out.println("p4");
 
-<<<<<<< HEAD
 System.out.println("código modificado");
 System.out.println("codigo atualizado de novo");
 System.out.println("P4 VAI ESTUDAR");
-=======
-
-System.out.println("código modificado");
-System.out.println("codigo atualizado de novo");
-System.out.println("P4 VAI ESTUDAR");
-
->>>>>>> main
     }
 }
